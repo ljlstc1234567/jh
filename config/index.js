24eb01a938fca58cfc4df74076a01a5bac02bb1e
ljlstc123456 <501939334@qq.com>
2018-02-09 22:16:54 +0800

    2018-2-9

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '10.0.4.206', // can be overwritten by process.env.HOST
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
     port: 8000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,10 +6,12 @@
     <swiper-slide><img src="http://www.jh-buy.com/appTpls/app/wapindex/banner/1/img/51cc1fe2482aab10aaf40190bc8f5549.jpg?0.10799478418561459" alt=""></swiper-slide>
     <!-- Optional controls -->
   </swiper>
+  <h3>憬弘特色</h3>
+  <h3>羊绒外套<router-link to="/">更多></router-link></h3>
   </div>
 </template>
 <script>
-  import 'swiper/dist/css/swiper.css'
+import 'swiper/dist/css/swiper.css'
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
 export default {
   data() {
@@ -39,9 +41,23 @@ export default {
 
 </script>
 <style lang="less" scoped>
+@import "../../static/variables.less";
 .myswiper{
   img{
     width: 100%;
+  }
+}
+h3{
+  padding:10px;
+  font-weight: normal;
+  text-align: left;
+  color:@main-color;
+  a{
+    display:inline-block;
+    padding:3px 0 0 20px;
+    float:right;
+    color:#888;
+    font-size:14px;
   }
 }
 </style>
